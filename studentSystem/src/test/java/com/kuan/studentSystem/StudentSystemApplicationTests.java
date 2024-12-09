6a2183addceb4511897ee47852b9719be46ba8aa
Yu Kuan Lin <zack871025@gmail.com>
2024-12-09 18:53:24 +1000

    make build fail

--- studentSystem/src/test/java/com/kuan/studentSystem/StudentSystemApplicationTests.java
+++ studentSystem/src/test/java/com/kuan/studentSystem/StudentSystemApplicationTests.java
@@ -1,5 +1,6 @@
 package com.kuan.studentSystem;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class StudentSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail("Oops test failed");
 	}
 
 }
